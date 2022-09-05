e3d6a116f3388c5abdc0a1ec074a6a201a7c1c3f
abc20210708 <gotqlc2670@naver.com>
2022-09-05 21:08:16 +0900

    강좌 14 | app bar 메뉴 아이콘 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -8,137 +8,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "BBANTO",
-      home: Grade(),
-
+        title: "Appbar",
+        theme: ThemeData(
+          primarySwatch: Colors.red
+        ),
+      home: MyPage(),
     );
   }
 }
 
-
-class Grade extends StatelessWidget {
-  const Grade({Key? key}) : super(key: key);
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text("BBANTO"),
-        backgroundColor: Colors.amber[700],
+        title: Text("Appbar icon menu"),
         centerTitle: true,
-        elevation: 0.0, //앱바 입체감 없애기
-      ),
-      body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/00.jpg"),
-                radius: 60.0,
-              ),
-            ),
-            Divider(
-              height: 60.0, //위와 아래 사이 간격이 합해서 60.0
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0, //끝에서부터 어느정도 떨어져야 할지 지정하는 속성
-            ),
-            Text("NAME",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text("BBANTO",
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text("BBANTO POWER LEVEL",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text("14",
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("using lightsaber",
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),
-                ),
-              ], //widget []
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("face here tattoo",
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ], //widget []
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text("fire flames",
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ], //widget []
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/03.jpg"),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ], //widget
-      ),
+        elevation: 0.0,
+        leading: IconButton(icon: Icon(Icons.menu), // leading //간단한 위젯이나 아이콘 등을 앱바 타이틀 왼쪽에 위치시키는 기능
+          onPressed: () {
+            print("menu button is clicked");
+          },
+        ),
+        actions: [
+          IconButton(icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print("Shopping button is clicked");
+            },
+          ),
+          IconButton(icon: Icon(Icons.search),
+            onPressed: () {
+              print("Search button is clicked");
+            },
+          ),
+        ],
       ),
     );
   }
 }
+
