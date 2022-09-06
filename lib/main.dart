95429a5d2ace2113e2afc0924b18083abea02dcd
abc20210708 <gotqlc2670@naver.com>
2022-09-06 20:50:16 +0900

    강좌 19 | 토스트 메세지 구현하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,9 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: "Snack bar",
         theme: ThemeData(
-          primarySwatch: Colors.red
+          primarySwatch: Colors.blue
         ),
       home: MyPage(),
     );
@@ -24,35 +26,32 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Snack Bar"),
+          title: Text("Toast message"),
           centerTitle: true,
         ),
-      body: MySnackBar(),
+        body:Center(
+          child: TextButton(
+            onPressed: (){
+              flutterToast();
+            },
+            child: Text("Toast",
+              style: TextStyle(color: Colors.black),
+            ),
+           style: TextButton.styleFrom(
+             backgroundColor: Colors.blue
+           ),
+          ),
+        ),
      );
   }
 }
 
-class MySnackBar extends StatelessWidget {
-  const MySnackBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Hello",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white
-                  ),
-                ),
-                  backgroundColor: Colors.teal,
-                  duration: Duration(milliseconds: 1000),
-                ));
-          },
-          child: Text("Show me")
-      ),
-    );
-  }
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: "Flutter",
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
 }
