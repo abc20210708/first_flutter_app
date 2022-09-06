bf5c228f70f0c31735de967fbd1d6276de5f6053
abc20210708 <gotqlc2670@naver.com>
2022-09-06 19:39:21 +0900

    강좌 18 | 스낵바와 BuildContext (1)

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Appbar",
+        title: "Snack bar",
         theme: ThemeData(
           primarySwatch: Colors.red
         ),
@@ -24,85 +24,26 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Appbar icon menu"),
+        title: Text("Snack Bar"),
         centerTitle: true,
-        elevation: 0.0,
-        actions: [
-          IconButton(icon: Icon(Icons.shopping_cart),// leading //간단한 위젯이나 아이콘 등을 앱바 타이틀 왼쪽에 위치시키는 기능
-            onPressed: () {
-              print("Shopping button is clicked");
-            },
-          ),
-          IconButton(icon: Icon(Icons.search),
-            onPressed: () {
-              print("Search button is clicked");
-            },
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage("assets/00.jpg"),
-                  backgroundColor: Colors.white,
-                ), //현재 사용자 이미지 가져오기
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage("assets/03.jpg"),
-                ),
-               /* CircleAvatar(
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage("assets/03.jpg"),
-                )*/
-              ], //한개이상의 다른 계정 이미지 추가
-                accountName: Text("BBANTO"), accountEmail: Text("bbanto@bbant.co.kr"),
-                onDetailsPressed: () {
-                  print("arrow is clicked");
-                },
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0)
-                )
+      body: Center(
+        //FlatButton 참고 블로그 https://learncom1234.tistory.com/24
+
+        child: TextButton(onPressed: ()
+        {
+          print("text button");
+        },
+            child: Text(
+              "Show me",
+              style: TextStyle(
+                color: Colors.white
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.home,
-              color: Colors.grey[850],
-              ),
-              title: Text("Home"),
-              onTap: (){
-                print("Home is clicked");
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text("Setting"),
-              onTap: (){
-                print("Setting is clicked");
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text("Q&A"),
-              onTap: (){
-                print("Q&A is clicked");
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],//[]
+            style: TextButton.styleFrom(
+            backgroundColor: Colors.red
         ),
+        )
       ),
     );
   }
