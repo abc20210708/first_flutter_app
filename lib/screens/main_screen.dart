110a01e866a8794525301ee76b4a6ca8075beeae
abc20210708 <gotqlc2670@naver.com>
2022-09-16 14:44:01 +0900

    조금 매운맛 강좌 20 | 채팅앱 UI 디자인 (2)

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -40,13 +40,147 @@ class _LoginSignUpScreenState extends State<LoginSignUpScreen> {
                             fontSize: 25,
                             color: Colors.white
                           ),
+                          children: [
+                            TextSpan(
+                              text: " to Yummy chat!",
+                              style: TextStyle(
+                                  letterSpacing: 1.0,
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ],
                         ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text("Signup to continue",
+                      style: TextStyle(
+                          letterSpacing: 1.0,
+                          color: Colors.white,
+                      ),
                     ),
                   ],
                 ),
               ),
           ),
-          )
+          ),
+          Positioned(
+            top: 180,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+            height: 280.0,
+              width: MediaQuery.of(context).size.width-40,
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 15,
+                  spreadRadius: 15
+                ),
+              ],
+            ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            isSignUpScreen = false;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text("LOGIN",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: !isSignUpScreen ? Palette.activeColor
+                                  : Palette.textColor1
+                            ),
+                            ),
+                            if(!isSignUpScreen)
+                            Container(
+                              margin: EdgeInsets.only(top: 3),
+                              height: 2,
+                              width: 55,
+                              color: Colors.orange,
+                            ),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            isSignUpScreen = true; //사용자가 메뉴를 선택했다
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Text("SIGNUP",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: isSignUpScreen ? Palette.activeColor
+                                      : Palette.textColor1
+                              ),
+                            ),
+                            if(isSignUpScreen)
+                            Container(
+                              margin: EdgeInsets.only(top: 3),
+                              height: 2,
+                              width: 55,
+                              color: Colors.orange,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    child: Form(
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.account_circle,
+                                  color: Palette.iconColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular((35.0),
+                                  )
+                                    ,),
+                                ),
+                                focusedBorder:  OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Palette.textColor1
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular((35.0),
+                                    )
+                                    ,),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ],
+              ),
+          ),
+          ),
         ],
       ),
     );
